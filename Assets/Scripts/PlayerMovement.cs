222e6d89f94d635f7e93066396a2db1899969f0f
Dylan <Dylanwebb5@hotmail.com>
2018-11-05 15:44:58 +1100

    Basic Player Movement + World
    Basic Player Movement implemented, world made, when player runs into red zone trigger battle level

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector3 input;
+    private Vector3 velocity;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        velocity = input * speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = velocity;
+
+
+        /*
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+        */
+    }
+}
