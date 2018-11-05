fd6730260534b8e09e247d0fe2afaf374fc8c911
Dylan <Dylanwebb5@hotmail.com>
2018-11-05 16:38:37 +1100

    Added In Enemy/Hero
    Added in Enemy and Hero stats and statemachines

---
+++ Assets/Scripts/BaseHero.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseHero
+{
+    public string name;
+
+    public float baseHp;
+    public float curHp;
+
+    public float baseMp;
+    public float curMp;
+
+    public int stamina;
+    public int interllect;
+    public int dexterity;
+    public int agility;
+
+
+
+
+}
