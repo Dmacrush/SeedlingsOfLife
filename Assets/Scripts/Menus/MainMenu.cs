dbb2333324eb971c70b3010f12cba003e162054f
Unknown <Dylanwebb5@hotmail.com>
2019-02-15 16:02:37 +1100

    Audio/MainMenu Implementation
    I added in main menu and audio stuff, options menu works too, however the resolution and full screen don't work in editor tests need to be done in build in later stage

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject playMenuHolder;
+    
+    public GameObject optionsMenuHolder;
+    public GameObject quitHolder;
+
+    public Slider[] volumeSliders;
+    public Toggle[] resolutionToggles;
+    public Toggle fullScreenToggle;
+    public int[] screenWidths;
+
+    int activeScreenResIndex;
+
+    private void Start()
+    {
+        activeScreenResIndex = PlayerPrefs.GetInt("screen res index");
+        bool isFullscreen = (PlayerPrefs.GetInt("fullscreen") == 1 ? true : false);
+
+        volumeSliders[0].value = AudioManager.instance.MasterVolumePercent;
+
+        volumeSliders[0].value = AudioManager.instance.MasterVolumePercent;
+        volumeSliders[1].value = AudioManager.instance.MusicVolumePercent;
+        volumeSliders[2].value = AudioManager.instance.SfxVolumePercent;
+
+        for (int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].isOn = 1 == activeScreenResIndex;
+
+        }
+
+        fullScreenToggle.isOn = isFullscreen;
+    }
+
+    public void PlayGame()
+    {
+        //put load level here when we finialise names
+        
+    }
+
+    public void DisplayPlayMenu()
+    {
+        playMenuHolder.SetActive(true);
+        optionsMenuHolder.SetActive(false);
+        quitHolder.SetActive(false);
+    }
+
+    public void DisplayQuitMenu()
+    {
+        playMenuHolder.SetActive(false);
+        optionsMenuHolder.SetActive(false);
+        quitHolder.SetActive(true);
+    }
+
+    public void DisPlayOptionsMenu()
+    {
+        playMenuHolder.SetActive(false);
+        quitHolder.SetActive(false);
+        optionsMenuHolder.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
+    public void SetScreenResolution(int i)
+    {
+        if (resolutionToggles[i].isOn)
+        {
+            activeScreenResIndex = i;
+            float aspectRatio = 16 / 9f;
+            Screen.SetResolution(screenWidths[i], (int)(screenWidths[i] / aspectRatio), false);
+            PlayerPrefs.SetInt("screen res index", activeScreenResIndex);
+            PlayerPrefs.Save();
+        }
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        for (int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].interactable = !isFullscreen;
+        }
+
+        if (isFullscreen)
+        {
+            Resolution[] allResolutions = Screen.resolutions;
+            Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+            Screen.SetResolution(maxResolution.width, maxResolution.height, true);
+        }
+        else
+        {
+            SetScreenResolution(activeScreenResIndex);
+        }
+
+        PlayerPrefs.SetInt("fullscreen", ((isFullscreen) ? 1 : 0));
+        PlayerPrefs.Save();
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Music);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Sfx);
+    }
+
+
+
+
+}
+
